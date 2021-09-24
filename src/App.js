3fd6394fad768249b44f02af905d95d2672b51e9
pyong <muanda1993@hotmail.com>
2021-09-24 01:04:41 +0100

    add event to map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Map, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import "./App.css";
+import data from "./angola.json";
 
 function App() {
+  const cityStyle = {
+    fillColor: "red",
+    color: "black",
+  };
+  const onEachCity =(city,layer)=>{
+    //city is the geojson data 
+    const provincia = city.properties.provincia
+    // the layer represents what is display on the screen
+    //bindPopup: allow to display a tooltip 
+    layer.bindPopup(provincia)
+
+    //add events
+    layer.on({
+      click: (e)=>{
+        e.target.setStyle({
+          fillColor: "green"
+        })
+      },
+      mouseout: (e)=>{
+        e.target.setStyle({
+          fillColor: "red"
+        })
+    }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map center={[-11.202692, 17.873887]} zoom={5.5} scrollWheelZoom={false}>
+      <GeoJSON style={cityStyle} data={data} onEachFeature={onEachCity}  />
+    </Map>
   );
 }
 
